31415bb57f91d2b81eab10d609eb25a7175d06f3
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2024-10-05 00:38:33 +0530

    Tesing vpc module; Tested select availability zones

--- main.tf
+++ main.tf
@@ -4,4 +4,5 @@ module "vpc" {
   project        = "expense"
   environment    = "dev"
   vpc_cidr_block = "10.0.0.0/16"
+  select_azs     = [1, 2]
 }
--- output.tf
+++ output.tf
@@ -5,3 +5,12 @@ output "vpc_id" {
 output "igw_id" {
   value = module.vpc.igw_id
 }
+
+# for debugging
+output "all_az_info" {
+  value = module.vpc.all_azs_info.names
+}
+
+output "selected_azs" {
+  value = module.vpc.selected_azs
+}
