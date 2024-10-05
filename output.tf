2fc83c65cca3779a8931e87f56f1cbbd84f0a7a3
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2024-10-05 15:26:56 +0530

    Tested database subnet group

--- output.tf
+++ output.tf
@@ -33,3 +33,7 @@ output "private_subnet_ids" {
 output "database_subnet_ids" {
   value = module.vpc.database_subnet_ids
 }
+
+output "db_subnet_group_id" {
+  value = module.vpc.db_subnet_group_id
+}
